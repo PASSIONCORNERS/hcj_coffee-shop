7b5830bf9c79f06d67b71a2ba175a8b6c2bc500e
Phong Truong <phongtruong@Phongs-iMac.local>
2019-09-08 14:11:15 +0700

    initial commit

---
+++ app/js/script.js
@@ -0,0 +1,62 @@
+// let logo = document.getElementById("logo");
+
+// function afterClick() {
+// 	this.classList.toggle("done");
+// }
+
+// logo.addEventListener("click", afterClick);
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+
+
+
+
+
+// -------------New Slider--------------
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:true,
+        navigation:false,
+        navigationText:["",""],
+        slideSpeed:1000,
+        singleItem:true,
+        transitionStyle:"fade",
+        autoPlay:true
+    });
+});
+
+
+
+
+
+
+
